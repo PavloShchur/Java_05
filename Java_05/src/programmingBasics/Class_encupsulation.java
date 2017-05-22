24a6a388447651bd4ad72612e88fa7017adeab10
Pavlo <pavloshchur@gmail.com>
2017-05-22 19:50:26 +0300

    Java_05

---
+++ Java_05/src/programmingBasics/Class_encupsulation.java
@@ -0,0 +1,12 @@
+package programmingBasics;
+
+public class Class_encupsulation {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.setAge(20);
+		person.setName("Bob");
+		System.out.println(person);
+	}
+
+}
---
+++ Java_05/src/programmingBasics/ShopBag.java
@@ -0,0 +1,9 @@
+package programmingBasics;
+
+public class ShopBag {
+
+	public static void main(String[] args) {
+
+	}
+
+}
